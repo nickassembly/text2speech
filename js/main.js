5e39ad04447b991e22f5955bf5a1806a6ea61c16
Nick Guerra <nickg@maliciousgames.com>
2019-09-09 20:50:47 -0500

    moved file structure

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Init speech synth api
+const synth = window.speechSynthesis;
+
+// DOM elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+    voices = synth.getVoices();
+    console.log(voices);
+}
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+}
