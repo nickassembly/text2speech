09b2e34082c1b199a9695f1da618241a217f498a
Nick Guerra <nickg@maliciousgames.com>
2019-09-10 21:05:56 -0500

    added speak function

--- js/main.js
+++ js/main.js
@@ -9,16 +9,97 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
 
 // Init voices array
 let voices = [];
 
 const getVoices = () => {
     voices = synth.getVoices();
-    console.log(voices);
+
+    // Loop through voices and create an option for each one
+    voices.forEach(voice => {
+        // Create an option element
+        const option = document.createElement('option');
+        // Fill option with voice and language
+        option.textContent = voice.name + '(' + voice.lang + ')';
+
+        // Set needed option attributes
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    });
 }
 
 getVoices();
 if (synth.onvoiceschanged !== undefined) {
     synth.onvoiceschanged = getVoices;
 }
+
+
+// Voice
+const speak = () => {
+
+    // Check if running
+    if (synth.speaking) {
+        console.error('Already Speaking...');
+        return;
+    }
+
+    if (textInput.value !== '') {
+        // Add background
+        body.style.background = '#141414 url(dist/img/wave.gif)';
+        body.style.backgroundRepeat = 'repeat-x';
+        body.style.backgroundSize = '100% 100%';
+
+        // Get speak text
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+        // Speak end
+        speakText.onend = e => {
+            console.log('Done speaking...');
+            body.style.background = '#141414';
+        }
+
+        // Speak error
+        speakText.onerror = e => {
+            console.error('Something went wrong...');
+        }
+
+        // Selected voice
+        const selectedVoice = voiceSelect.selectedOptions[0]
+            .getAttribute('data-name');
+
+        // Loop through voices
+        voices.forEach(voice => {
+            if (voice.name === selectedVoice) {
+                speakText.voice = voice;
+            }
+        });
+
+        // Set pitch and rate
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+
+        // Speak
+        synth.speak(speakText);
+    }
+};
+
+// Event Listeners
+
+// Text form submit
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener('change', e => rateValue.textContent = rate.value);
+
+// Pitch value change
+pitch.addEventListener('change', e => pitchValue.textContent = rate.value);
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
